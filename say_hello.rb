d9a231b810f269df09ed548fef39c1f845028dc0
Autumn Williams <awilliams3418@gmail.com>
2020-04-18 02:59:40 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,17 @@
 # Build your say_hello method here
+
+def say_hello
+  puts "Hello Ruby programmer!" 
+end
+
+def say_hello(name)
+  puts "Hello #{name}"
+end
+
+def say_hello(name = "Ruby programmer!")
+  puts "Hello #{name}"
+end
+
+
+
+
